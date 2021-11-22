31fab3dbec83587beb0b135d479d33a4df84e4cf
MrGcGamer <32813275+MrGcGamer@users.noreply.github.com>
2021-11-22 02:01:24 +0100

    Changed and thingies around

--- Tweak/Eneko.h
+++ Tweak/Eneko.h
@@ -1,84 +1,104 @@
 #import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
 #import <sys/utsname.h>
-#import "GcUniversal/GcImagePickerUtils.h"
+#import <GcUniversal/GcImagePickerUtils.h>
+#import <GcUniversal/HelperFunctions.h>
 #import <Cephei/HBPreferences.h>
 
 HBPreferences* preferences = nil;
 
-BOOL enabled = NO;
-
-BOOL isLockscreenVisible = NO;
-BOOL isHomescreenVisible = NO;
-BOOL isControlCenterVisible = NO;
-BOOL isInCall = NO;
 BOOL screenIsOn = YES;
 BOOL isOnLowPower = NO;
 BOOL supportsLowPowerMode = NO;
 
 AVQueuePlayer* playerLS = nil;
-AVPlayerItem* playerItemLS = nil;
 AVPlayerLooper* playerLooperLS = nil;
 AVPlayerLayer* playerLayerLS = nil;
 UIView* dimBlurViewLS = nil;
-UIVisualEffectView* blurViewLS = nil;
-UIBlurEffect* blurLS = nil;
-UIView* dimViewLS = nil;
+
+UIView *LSContainerView = nil;
 
 AVQueuePlayer* playerHS = nil;
-AVPlayerItem* playerItemHS = nil;
 AVPlayerLooper* playerLooperHS = nil;
 AVPlayerLayer* playerLayerHS = nil;
 UIView* dimBlurViewHS = nil;
-UIVisualEffectView* blurViewHS = nil;
-UIBlurEffect* blurHS = nil;
-UIView* dimViewHS = nil;
 
 AVQueuePlayer* playerCC = nil;
-AVPlayerItem* playerItemCC = nil;
 AVPlayerLooper* playerLooperCC = nil;
 AVPlayerLayer* playerLayerCC = nil;
 UIView* dimBlurViewCC = nil;
-UIVisualEffectView* blurViewCC = nil;
-UIBlurEffect* blurCC = nil;
-UIView* dimViewCC = nil;
 
 // lockscreen
 BOOL enableLockscreenWallpaperSwitch = NO;
-NSString* lockscreenVolumeValue = @"0.0";
-NSString* lockscreenBlurAmountValue = @"0.0";
-NSString* lockscreenBlurModeValue = @"0";
-NSString* lockscreenDimValue = @"0.0";
-NSString* lockscreenOpacityValue = @"1.0";
+CGFloat lockscreenVolumeValue = 0.0;
+CGFloat lockscreenBlurAmountValue = 0.0;
+NSInteger lockscreenBlurModeValue = 0;
+CGFloat lockscreenDimValue = 0.0;
+CGFloat lockscreenOpacityValue = 1.0;
 
 // homescreen
 BOOL enableHomescreenWallpaperSwitch = NO;
-NSString* homescreenVolumeValue = @"0.0";
-NSString* homescreenBlurAmountValue = @"0.0";
-NSString* homescreenBlurModeValue = @"0";
-NSString* homescreenDimValue = @"0.0";
-NSString* homescreenOpacityValue = @"1.0";
+CGFloat homescreenVolumeValue = 0.0;
+CGFloat homescreenBlurAmountValue = 0.0;
+NSInteger homescreenBlurModeValue = 0;
+CGFloat homescreenDimValue = 0.0;
+CGFloat homescreenOpacityValue = 1.0;
 
 // control center
 BOOL enableControlCenterWallpaperSwitch = NO;
-NSString* controlCenterVolumeValue = @"0.0";
-NSString* controlCenterBlurAmountValue = @"0.0";
-NSString* controlCenterBlurModeValue = @"0";
-NSString* controlCenterDimValue = @"0.0";
-NSString* controlCenterOpacityValue = @"1.0";
+CGFloat controlCenterVolumeValue = 0.0;
+CGFloat controlCenterBlurAmountValue = 0.0;
+NSInteger controlCenterBlurModeValue = 0;
+CGFloat controlCenterDimValue = 0.0;
+CGFloat controlCenterOpacityValue = 1.0;
 
 // miscellaneous
 BOOL muteWhenMusicPlaysSwitch = YES;
 BOOL hideWhenLowPowerSwitch = YES;
 
-@interface CSCoverSheetViewController : UIViewController
-- (void)adjustFrame;
-@end
-
-@interface SBIconController : UIViewController
-- (void)adjustFrame;
+@interface MTMaterialView : UIView
+@property (nonatomic, assign) CGFloat weighting;
 @end
 
 @interface CCUIModularControlCenterOverlayViewController : UIViewController
-- (void)adjustFrame;
+@property (nonatomic,readonly) MTMaterialView * overlayBackgroundView;
+@end
+
+@interface SBFWallpaperView : UIView
+@property (retain, nonatomic) UIView *contentView; // ivar: _contentView
+
+// NEW
+- (void)setupEneko:(NSInteger)isHome ;
+@end
+
+@interface SBWallpaperViewController : UIViewController
+@property (retain, nonatomic) SBFWallpaperView *homescreenWallpaperView; // ivar: _homescreenWallpaperView
+@property (retain, nonatomic) SBFWallpaperView *lockscreenWallpaperView; // ivar: _lockscreenWallpaperView
+@property (retain, nonatomic) SBFWallpaperView *sharedWallpaperView; // ivar: _sharedWallpaperView
+@end
+
+@interface SBWallpaperEffectViewBase : UIView
+@property (nonatomic,retain) UIView*/*<_SBFakeBlur>*/ blurView;
+@end
+
+@interface SBWallpaperEffectView : SBWallpaperEffectViewBase
+@end
+
+@interface SBCoverSheetPrimarySlidingViewController : UIViewController
+@property (nonatomic,retain) SBWallpaperEffectView * panelWallpaperEffectView;
+@end
+
+@interface SBCoverSheetPresentationManager : NSObject
++(id)sharedInstance;
+-(BOOL)isVisible;
+@end
+
+@interface SBControlCenterController : NSObject
++(id)sharedInstance;
+-(BOOL)isVisible;
+@end
+
+@interface SBTelephonyManager : NSObject
++(id)sharedTelephonyManager;
+-(BOOL)inCall;
 @end
